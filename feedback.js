ac9db5cb4ae6085d768fb09a681e78dddfc01761
Renato Guimarães <renatoguidev@gmail.com>
2022-07-15 19:34:45 -0300

    1.0

--- feedback.js
+++ feedback.js
@@ -1,3 +1,17 @@
+let container1var = document.querySelector("div#container1")
+let container2var = document.querySelector("div#container2")
+let respostavar = document.querySelector("div#resposta")
+
 function clicar () {
-    
+    var valor = document.querySelector('input[name="feedback"]:checked')
+        container1var.style.display = "none"
+        container2var.style.display = "block"
+        respostavar.innerHTML = `You selected ${valor.value} out of 5`
 }
+
+
+
+
+
+
+    
