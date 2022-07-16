dbe915b1991dc9363a5002b5000be688e3746bcd
Renato Guimarães <renatoguidev@gmail.com>
2022-07-16 15:22:27 -0300

    1.0

--- feedback.js
+++ feedback.js
@@ -1,15 +1,27 @@
-let container1var = document.querySelector("div#container1")
-let container2var = document.querySelector("div#container2")
-let respostavar = document.querySelector("div#resposta")
+let container1var = document.querySelector("#container1")
+let container2var = document.querySelector("#container2")
+let respostavar = document.querySelector("#resposta")
 
-function clicar () {
+function findSelector () {
     var valor = document.querySelector('input[name="feedback"]:checked')
-    container1var.style.display = "none"
-    container2var.style.display = "block"
-    respostavar.innerHTML = `You selected ${valor.value} out of 5`
+    if (valor != null) {
+        respostavar.innerHTML = `You selected ${valor.value} out of 5`
+        return 1 
+    } else {
+        window.alert ('[ERRO] Verifique seus dados novamente!')
+        return 0
+    }
 }
 
-
+function clicar () {
+    if (findSelector () == 0) {
+        return 
+    } else {
+        findSelector ()
+        container1var.style.display = "none"
+        container2var.style.display = "block"
+    }
+}
 
 
 
